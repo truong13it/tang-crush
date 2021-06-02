a79102289f084d9cbfdb06265d544a06ef17f73d
truong13it <85246192+truong13it@users.noreply.github.com>
2021-06-02 23:38:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    introTitle : 'Bae à!' ,
-    introDesc : Thường  lệ  vốn  trái  đất
+    introTitle : 'Em à!' ,
+    introDesc : Thường   lệ   vốn   trái   đất
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
